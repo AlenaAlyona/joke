29778dffd3e986d16894a82c201d9858837a2c9d
AlenaAlyona <izakson91@gmail.com>
2020-08-28 11:54:14 +0200

    wrote a joke

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const page = `<html>
+<head>
+  <title>Alena Izakson</title>
+</head>
+
+<body>
+  <h1>If your boss is getting you down,</h1>
+  <p>look at him through the prongs of a fork and imagine him in jail.</p>
+  <p>Ricky Gervais</p>
+</body>
+</html>`
+
+
+
+app.get(
+    '/',
+    (request, response) => { 
+    response.send(page)
+    }
+)
+
+const port = 8900
+
+  
+  app.listen(
+    port,
+    () => console.log(`Listening on :${port}`)
+  )
+
